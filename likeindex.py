e37f39ae80e39b8f166cd138da26291ee55cc1c3
Christan Grant <christangrant@gmail.com>
2010-10-30 04:33:37 -0400

    added a login/log out funtionality with a facebook button

--- likeindex.py
+++ likeindex.py
@@ -1,19 +1,26 @@
+import os
 from google.appengine.api import users
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
+from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp.util import run_wsgi_app
 
 class MainHandler(webapp.RequestHandler):
 	def get(self):
-		user = users.get_current_user()
 		
-		if user:
-			self.response.headers['Content-Type'] = 'text/plain'
-			self.response.out.write('Hello, ' + user.nickname())
+		if users.get_current_user():
+			url = users.create_logout_url(self.request.uri)
+			url_linktext = u'Logout'
 		else:
-			self.redirect(users.create_login_url(self.request.uri))
+			url = users.create_login_url(self.request.uri)
+			url_linktext = u'Login'
 
-
+		template_values = {
+				'url': url,
+				'url_linktext': url_linktext,
+				}
+		path = os.path.join(os.path.dirname(__file__), 'templates/index.html')
+		self.response.out.write(template.render(path, template_values))
 
 def main():
 	application = webapp.WSGIApplication(
