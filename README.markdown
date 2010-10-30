baf48d5c8f64cb62d398e84058602696f75dd1b3
Christan Grant <christangrant@gmail.com>
2010-10-30 17:39:38 -0400

    added files I forgot to commit

--- README.markdown
+++ README.markdown
@@ -4,6 +4,6 @@ This allows you to keep track of all user favorite stuff across the web.
 
 Favorite stuff includes:
 
-* Facebook Likes
-* Twitter Favorites
-* Youtube Favorites 
+* Facebook Likes (in progress)
+* Twitter Favorites (not started)
+* Youtube Favorites (not started)
--- likeindex.py
+++ likeindex.py
@@ -1,16 +1,35 @@
+import facebook
 import os
+
 from google.appengine.api import users
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
 from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp.util import run_wsgi_app
 
+import models
+try:
+	import res
+except:
+	pass
+
+_fbsecret = res.keys['fbsecret'] if res else ""
+_fbappid = res.keys['fbappid'] if res else ""
+
 class MainHandler(webapp.RequestHandler):
 	def get(self):
+		fbuser = facebook.get_user_from_cookie(self.request.cookies,
+				_fbappid, _fbsecret)
 		
 		if users.get_current_user():
 			url = users.create_logout_url(self.request.uri)
 			url_linktext = u'Logout'
+			
+			if fbuser:
+				graph = facebook.GraphAPI(user["oauth_access_token"])
+				profile = graph.get_object("me")
+				friends = graph.get_connections("me", "friends")
+			
 		else:
 			url = users.create_login_url(self.request.uri)
 			url_linktext = u'Login'
